4fae9511da3aa3a05a16c4f1d0bb4fc7401bba5d
llrrdd <a@ac.com>
2014-11-09 19:20:21 -0500

    Create ReduceContext class

---
+++ MapReduce_Library/ReduceContext.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+public class ReduceContext<KEYIN, VALUEIN, KEYOUT, VALUEOUT>
+    extends Context<KEYIN, VALUEIN, KEYOUT, VALUEOUT> {
+
+    public KEYIN getCurrentKey() {
+
+    }
+
+    public VALUEIN getCurrentValue() {
+
+    }
+
+    public Iterable<VALUEIN> getCurrentValues() {
+        
+    }
+
+    public boolean nextKeyValue() {
+
+    }
+}
