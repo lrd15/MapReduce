da24b2176897b59739e78f9917068f39aede4567
abby <abby@ubuntu.ubuntu-domain>
2014-11-22 13:57:08 -0500

    test

--- system/TaskTracker.java
+++ system/TaskTracker.java
@@ -144,8 +144,9 @@ public class TaskTracker extends Thread {
 			Partitioner partitioner = (Partitioner) job.getPartitionerClass().newInstance();
 			RecordReader<Long, String> reader = inputFormat.getRecordReader(job, JobTracker.MAPIN_DIR, inputSplit);
 			String[] intermediateFiles = inputFormat.getFilenames();
+			System.out.println("intermediateFiles: " + intermediateFiles);
 			for (int i = 0; i < filenames.length; i++)
-				filenames[i] = intermediateFiles[i];
+				filenames[i] = new String(intermediateFiles[i]);
 			MapContext mapContext = new MapContext<Long, String, String, String>(job, reader, partitioner);
 			Mapper mapper = (Mapper) job.getMapperClass().newInstance();
 			mapper.run(mapContext);
